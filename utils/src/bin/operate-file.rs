de1d6326446090ebb641a661103006cbfa3e6dd5
zhangxuan2011 <zx20110412@outlook.com>
2026-02-14 16:55:55 +0800

    update: 更新了Cargo metadata

--- utils/src/bin/operate-file.rs
+++
@@ -1,8 +0,0 @@
-use proka_fs::{FileSystem, init_block_device};
-
-fn main() {
-    let bd = init_block_device("/Users/zhangxuan/Desktop/proka.img").unwrap();
-    let mut fs = FileSystem::mount(bd);
-
-    fs.mkfile(0, "test.txt").unwrap();
-}
